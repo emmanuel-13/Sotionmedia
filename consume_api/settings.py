cde0136bd8efbee91cfd50e293820db3e374a835
emmanuel <jackoliver023@gmail.com>
2024-01-08 15:20:35 +0100

    updated settings

--- consume_api/settings.py
+++ consume_api/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "admin_static")
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static")
 # ]
@@ -163,6 +163,6 @@ JAZZMIN_SETTINGS = {
 }
 
 
-MEDIA_URL = "/media/"
+MEDIA_URL = "media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
